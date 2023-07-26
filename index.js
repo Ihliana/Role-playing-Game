62375464a6970ad346d5c92599f5900e0652835c
Ihliana <Ihnatco@protonmail.com>
2023-07-26 06:26:12 +0200

    Moving to objects
    create objects for characters and create renderCharacter() function

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
 
-document.getElementById('hero').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container">
-            <div class="dice"> 6 </div>
-        </div>
-    </div>     
-`
+const hero = {
+    elementId: "hero", 
+    name: "Wizard", 
+    avatar: "images/wizard.png", 
+    health: 60,
+    diceRoll: 6
+
+}
+
+const monster = {
+    elementId: "monster", 
+    name: "Orc", 
+    avatar: 'images/orc.png', 
+    health: 10,
+    diceRoll: 4
+}
 
 
-document.getElementById("monster").innerHTML = `
 
-    <div class="character-card">
-        <h4 class="name"> Orc </h4>
-        <img class="avatar" src="images/orc.png"/>
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container">
-            <div class="dice"> 4 </div>
-        </div>
-    </div>
-`
+function renderCharacter(data){
+    /*
+    Challenge:
+        1. Make renderCharacter render out the character 
+           from the object it is passed.
+    */
+
+    return  document.getElementById(data.elementId).innerHTML = `
+        <div class="character-card">
+            <h4 class="name"> ${data.name} </h4>
+            <img class="avatar" src=${data.avatar} />
+            <p class="health">health: <b> ${data.health} </b></p>
+            <div class="dice-container">
+                <div class="dice"> ${data.diceRoll} </div>
+            </div>
+
+        </div>  
+    `
+
+    }
+    
+    renderCharacter(hero)
+    renderCharacter(monster)
+
