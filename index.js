c3db1a274f7adbfc8e7740d40196cfe635049a67
Ihliana <Ihnatco@protonmail.com>
2023-08-01 09:51:45 +0200

    UX - pause time
    Add a pause of 1 second between a monster dying and
    another monster taking it's place.

--- index.js
+++ index.js
@@ -4,50 +4,20 @@ import Character from './Character.js'
 
 let monstersArray = ["orc", "demon", "goblin"];
 
-/*
-Challenge
-1. Create a function called getNewMonster.
-2. Write logic inside the function that takes the first 
-monster from monstersArray and extracts that monster's 
-data from characterData.
-3. Save that data to a new const called nextMonsterData.
-**hint.md for help!!**
-*/
-
-/*
-Challenge
-1. Make it so getNewMonster returns a new instance of Character. Think
-what argument you should be passing. If there are no more monsters in the 
-array, getNewMonster should return an empty object {}.
-2. Down near the bottom of the file, set a new variable "monster" equal 
-to our new function getNewMonster.
-3. Delete any code we no longer need.
-- The app will still be broken - don't worry for now!
-**hint.md for help!!**
-*/
-
-
-/*
-Challenge
-1. See if you can get the app to work with just 
-one monster again.
-**hint.md for help!!**
-*/
-
-
-/*
-Challenge
-1. Change the attack function so that when a monster dies, 
-the next monster replaces it. If there are no more monsters,
-call endGame(). 
-2. Make sure that endGame() still gets called if the wizard
-is killed.
-*/
 
 function getNewMonster() {
     const nextMonsterData = characterData[monstersArray.shift()]
     return nextMonsterData ? new Character(nextMonsterData) : {}
 }
+
+
+/*
+Challenge
+1. Add a pause of 1 second between a monster dying and
+another monster taking it's place.
+2. Add a pause of 1.5 seconds between the last monster 
+or the wizard dying, and the endMessage being displayed.
+*/
 
 
 
@@ -65,8 +35,10 @@ function attack(){
     }
      else if(monster.dead){
         if(monstersArray.length > 0){
-            monster = getNewMonster()
-            render()
+            setTimeout(() => {
+                monster = getNewMonster()
+                render()
+            },1000)
         } else {
             endGame()
         }
@@ -82,12 +54,16 @@ function endGame(){
                     : "The Orc is Victorious"
 
     const endEmoji = wizard.health > 0 ? "🔮" : "☠️" 
-    document.body.innerHTML = ` 
-        <div class="end-game">
-             <h2>Game Over</h2> 
-             <h3>${endMessage}</h3>
-             <p class="end-emoji">${endEmoji}</p>
-        </div>`
+
+    setTimeout(() => {
+        document.body.innerHTML = ` 
+            <div class="end-game">
+                 <h2>Game Over</h2> 
+                 <h3>${endMessage}</h3>
+                 <p class="end-emoji">${endEmoji}</p>
+            </div>`
+
+    }, 1500)
 
 
 }
